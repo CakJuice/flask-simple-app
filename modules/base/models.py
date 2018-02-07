af9db3e04448f057f1cdde4d612f338fe2d2a09a
Cak Juice <hd.brandoz@gmail.com>
2018-02-08 05:38:17 +0700

    add docs

--- modules/base/models.py
+++ modules/base/models.py
@@ -10,12 +10,22 @@ class User(db.Model, BaseModel):
 	__tablename__ = 'cj_base_user'
 
 	def generate_slug(context):
+		"""
+		# Generate slug from name
+		# @param context (class): class object
+		# @return (string / None): result from helpers.generate_slug
+		"""
 		name = context.current_parameters.get('name')
 		if name:
 			return slugify(User, name)
 		return None
 
 	def generate_verify_code(context):
+		"""
+		# Generate random verify code after user signup
+		# @param context (class): class object
+		# @result (string): random result
+		"""
 		char_list = string.ascii_uppercase + string.digits + string.ascii_lowercase
 		return ''.join(random.choice(char_list) for _ in range(32))
 
@@ -39,18 +49,39 @@ class User(db.Model, BaseModel):
 
 	@staticmethod
 	def make_password(plaintext):
+		"""
+		# Create hash password from plaintext
+		# @param plaintext (string): plaintext which to be hashed
+		# @return (string): hash result
+		"""
 		return argon2.generate_password_hash(plaintext)
 
 	def check_password(self, raw_password):
+		"""
+		# Verify input password with password_hash
+		# @param self: Instantiate object
+		# @param raw_password (string): input password which to be verified
+		# @return (boolean): verify result
+		"""
 		return argon2.check_password_hash(self.password_hash, raw_password)
 
 	@staticmethod
 	def authenticate(email, password):
+		"""
+		# Check user when login
+		# @param email (string): input email from user
+		# @param password (string): input password from user
+		# @return (recordset / boolean): auth result, if user found then return user data, else return false
+		"""
 		user = User.query.filter_by(email=email).first()
 		if user and user.check_password(password):
 			return user
 		return False
 
 	def set_verified(self):
+		"""
+		# Update data status user when user verified account
+		# @param self: Instantiate object
+		"""
 		self.status = 1
 		self.save()
