58567e15608598d60ebeae7095bc738f929d7589
Hendra Juice <hd.brandoz@gmail.com>
2018-02-08 14:50:49 +0700

    add docs

--- config_example.py
+++ config_example.py
@@ -1,15 +1,18 @@
 import os
 
 class Configuration(object):
+	# flask config
 	APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
 	DEBUG = True
 	STATIC_DIR = os.path.join(APPLICATION_DIR, 'static')
 	IMAGES_DIR = os.path.join(STATIC_DIR, 'images')
 	SECRET_KEY = 'random_secret_key'
 
+	# flask_sqlalchemy config
 	SQLALCHEMY_DATABASE_URI = 'your_database_uri'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+	# flask_mail config
 	MAIL_SERVER = 'your_mail_server'
 	MAIL_PORT = 465
 	MAIL_USE_SSL = True
