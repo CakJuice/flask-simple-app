d92e5ce5dfabb0073f97758d26c42541a8234155
Hendra Juice <hd.brandoz@gmail.com>
2018-02-08 13:25:34 +0700

    bug fix

--- modules/base/models.py
+++ modules/base/models.py
@@ -5,36 +5,25 @@
 # @author: @CakJuice <hd.brandoz@gmail.com>
 """
 
-import random
-import string
 from datetime import datetime
 
 from app import app, db, argon2
 from models import BaseModel
-from helpers import generate_slug as slugify
+from helpers import generate_random_string, generate_slug as slugify
 
 class User(db.Model, BaseModel):
 	__tablename__ = 'cj_base_user'
 
-	def generate_slug(context):
+	def generate_slug(self):
 		"""
 		# Generate slug from name
-		# @param context (class): class object
+		# @param self (class): class object
 		# @return (string | None): result from helpers.generate_slug
 		"""
-		name = context.current_parameters.get('name')
+		name = self.current_parameters.get('name')
 		if name:
 			return slugify(User, name)
 		return None
-
-	def generate_verify_code(context):
-		"""
-		# Generate random verify code after user signup
-		# @param context (class): class object
-		# @result (string): random result
-		"""
-		char_list = string.ascii_uppercase + string.digits + string.ascii_lowercase
-		return ''.join(random.choice(char_list) for _ in range(32))
 
 	id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String(128), nullable=False, unique=True)
@@ -43,13 +32,13 @@ class User(db.Model, BaseModel):
 	slug = db.Column(db.String(128), nullable=False, unique=True, default=generate_slug)
 	status = db.Column(db.SmallInteger, default=0, doc="1 = active, 0 = need activated, -1 = non-active")
 	is_admin = db.Column(db.Boolean, default=False)
-	verify_code = db.Column(db.String(32), default=generate_verify_code)
+	verify_code = db.Column(db.String(32), default=generate_random_string(32))
 	created_at = db.Column(db.DateTime, default=datetime.now)
 	updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 	last_request_at = db.Column(db.DateTime)
 
 	def __init__(self, *args, **kwargs):
-		return super(User, self).__init__(*args, **kwargs)
+		super(User, self).__init__(*args, **kwargs)
 
 	def __str__(self):
 		return '<User: {}>'.format(self.name)
